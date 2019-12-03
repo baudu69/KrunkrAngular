3ec9ef9744889b7a708e8bdfe0d34800393089e6
baudu69 <bastien.audu@lmdsio.fr>
2019-12-03 10:48:12 +0100

    cdxcxc

--- src/app/service/visite.service.ts
+++ src/app/service/visite.service.ts
@@ -11,7 +11,7 @@ export class VisiteService {
 
   constructor(private http: HttpClient) { }
   public chargerVisites(): Observable<Visite[]> {
-    const url = 'http://localhost/gailleton/krunkr/public/api/visite/getAllVisite';
+    const url = environment.apiURL + '/visite/getAllVisite';
     return this.http.get<Visite[]>(url).pipe();
   }
 }
